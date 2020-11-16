d10b6ec5498c23317d09df55c8c3a48b1a04601b
Zümrüd-ü Anka <osmandurdag@hotmail.com>
2020-11-17 01:16:47 +0300

    added output save operations, drone videos

--- main.py
+++ main.py
@@ -36,8 +36,21 @@ if __name__ == '__main__' :
     if fromCam:
         video = cv2.VideoCapture(0)
     else:
-        video = cv2.VideoCapture("./resources/inputs/videos/street.mp4")
+        fileName = "street.mp4"
+        video = cv2.VideoCapture(f"./resources/inputs/videos/{fileName}")
     
+    frame_width = int(video.get(3))
+    frame_height = int(video.get(4))
+
+    size = (frame_width, frame_height)
+
+    output = cv2.VideoWriter(
+        f'./resources/outputs/videos/{fileName}',
+        cv2.VideoWriter_fourcc(*'MJPG'), 
+        30,
+        size
+    ) 
+
     # Exit if video not opened.
     if not video.isOpened():
         print("Could not open video")
@@ -48,21 +61,25 @@ if __name__ == '__main__' :
     if not ok:
         print('Cannot read video file')
         sys.exit()
-    
-    # Define an initial bounding box
-    bbox = (287, 23, 86, 320)
-
+   
     # Uncomment the line below to select a different bounding box
     bbox = cv2.selectROI(frame, False)
+
+    firstFrame = frame
+    grayFirstFrame = cv2.cvtColor(firstFrame, cv2.COLOR_RGB2GRAY)
+    firstBBox = bbox
+
     # Initialize tracker with first frame and bounding box
     ok = tracker.init(frame, bbox)
+
+    okCount = 0
+    accuracyResultArray = []
 
     while True:
         # Read a new frame
         ok, frame = video.read()
         if not ok:
             break
-        
         # Start timer
         timer = cv2.getTickCount()
 
@@ -77,13 +94,29 @@ if __name__ == '__main__' :
             # Tracking success
             p1 = (int(bbox[0]), int(bbox[1]))
             p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
-            cv2.rectangle(frame, p1, p2, (255,0,0), 2, 1)
+            cv2.rectangle(frame, p1, p2, (255, 0, 0), 2, 1)
+
+            output.write(frame)
+
+            ### Calculate Accuracy
+
+            grayFrame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+            dp = 0
+            for i in range(int(bbox[0]), int(bbox[0] + bbox[2])):
+            	for j in range(int(bbox[1]), int(bbox[1] + bbox[3])):
+                    if grayFirstFrame[i][j] == grayFrame[i][j]:
+                        dp += 1
+            f1 = dp / (firstBBox[2] * firstBBox[3])
+            cv2.putText(frame, "Accuracy Result : " + str(f1), (100,100), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50,170,50), 2)
+            accuracyResultArray.append(f1)
+            print(f1)
+
         else :
             # Tracking failure
-            cv2.putText(frame, "Tracking failure detected", (100,80), cv2.FONT_HERSHEY_SIMPLEX, 0.75,(0,0,255),2)
+            cv2.putText(frame, "Tracking failure detected", (100,80), cv2.FONT_HERSHEY_SIMPLEX, 0.75,(0,0,255), 2)
 
         # Display tracker type on frame
-        cv2.putText(frame, tracker_type + " Tracker", (100,20), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50,170,50),2)
+        cv2.putText(frame, tracker_type + " Tracker", (100,20), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50,170,50), 2)
     
         # Display FPS on frame
         cv2.putText(frame, "FPS : " + str(int(fps)), (100,50), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50,170,50), 2)
@@ -94,3 +127,10 @@ if __name__ == '__main__' :
         # Exit if ESC pressed
         k = cv2.waitKey(1) & 0xff
         if k == 27 : break
+
+# write objects 
+video.release() 
+output.release() 
+    
+# Closes all the frames 
+cv2.destroyAllWindows()
